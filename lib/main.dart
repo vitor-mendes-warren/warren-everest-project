e87b4dea313eb7d7384e913291c10b9b201621a6
Vitor Mendes <vitor6890@gmail.com>
2022-09-09 01:29:31 -0300

    [feat] wallet widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:js';
 
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:warren_task_one/wallet_page/wallet_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:warren_task_one/home_page/home_page.dart';
 
 void main() {
   runApp(DevicePreview(builder: (context) {
@@ -15,11 +16,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ProviderScope(
+        child: MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
       home: const WalletPage(),
-    );
+    ));
   }
 }
