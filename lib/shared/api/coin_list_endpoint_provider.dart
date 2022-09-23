18010faf7c423ac03ec46cb4d7953f603c7945aa
Vitor Mendes <vitor6890@gmail.com>
2022-09-23 11:44:43 -0300

    change : provider name

--- lib/shared/api/coin_list_endpoint_provider.dart
+++ lib/shared/api/coin_list_endpoint_provider.dart
@@ -3,7 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'coin_prices_endpoint.dart';
 
-final coinlistEndpointProvider = Provider(((ref) {
+final coinEndpointProvider = Provider(((ref) {
   final dio = Dio(BaseOptions(baseUrl: 'https://api.coingecko.com/api/v3'));
   return CoinEndpoint(dio);
 }));
--- lib/shared/repository/coin_prices_repository_provider.dart
+++ lib/shared/repository/coin_prices_repository_provider.dart
@@ -3,5 +3,5 @@ import 'package:warren_task_one/shared/api/coin_list_endpoint_provider.dart';
 import 'package:warren_task_one/shared/repository/coin_prices_repository.dart';
 
 final coinPricesRepositoryProvider = Provider((ref) {
-  return CoinPricesRepository(coinEndpoint: ref.read(coinlistEndpointProvider));
+  return CoinPricesRepository(coinEndpoint: ref.read(coinEndpointProvider));
 });
